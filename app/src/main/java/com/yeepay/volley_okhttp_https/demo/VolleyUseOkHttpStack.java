236a03eafb4d3127d925eae25046d326ba221afa
ztzt15 <496235592@qq.com>
2015-12-03 10:29:37 +0800

    update VolleyUseOkHttpStack.java

--- app/src/main/java/com/yeepay/volley_okhttp_https/demo/VolleyUseOkHttpStack.java
+++ app/src/main/java/com/yeepay/volley_okhttp_https/demo/VolleyUseOkHttpStack.java
@@ -15,17 +15,10 @@ import java.net.URL;
 
 public class VolleyUseOkHttpStack extends HurlStack {
     private OkHttpClient okHttpClient;
-    /**
-     * Create a OkHttpStack with default OkHttpClient.
-     */
     public VolleyUseOkHttpStack() {
         this(new OkHttpClient());
     }
 
-    /**
-     * Create a OkHttpStack with a custom OkHttpClient
-     * @param okHttpClient Custom OkHttpClient, NonNull
-     */
     public VolleyUseOkHttpStack(OkHttpClient okHttpClient) {
         this.okHttpClient = okHttpClient;
     }
